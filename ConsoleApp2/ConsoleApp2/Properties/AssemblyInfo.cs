28fa1e35b52ae7c30769dbd2f1fc5fa19027d084
Kyusung98 <sgs3898@gmail.com>
2024-01-26 14:07:35 +0900

    Revert "[C#] Variable/Type"
    This reverts commit 57dc5a86

--- ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
+++ ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
@@ -1,25 +1,11 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp2")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp2")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+﻿
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7BDB66CA-55FF-4D66-9974-A5127332887F")]
+[assembly: Guid("A2A6BAB6-E416-4EB8-9C07-95FA9BFB1656")]
 
 // Version information for an assembly consists of the following four values:
 //
