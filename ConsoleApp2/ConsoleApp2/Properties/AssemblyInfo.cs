bbf1cf377a4ec7d2c20a2cab7e0e6c92d5591535
Kyusung98 <sgs3898@gmail.com>
2024-01-26 14:13:15 +0900

    [C#] Variable/Type

--- ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
+++ ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
@@ -2,6 +2,10 @@
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
