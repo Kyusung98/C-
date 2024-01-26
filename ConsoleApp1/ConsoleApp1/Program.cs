3a87c1f25a9bb849d178c46e2c31a29456e9d0b5
Kyusung98 <sgs3898@gmail.com>
2024-01-26 13:24:22 +0900

    [C#] make project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
+    class Car
+    {
+        private string handle = "AMODEL";
+        public void carRun()
+        {
+            Console.WriteLine("자동차를 움직인다.");
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
         {
+            System.Console.WriteLine("System을 이용한 호출");
+            Console.WriteLine("하이");
         }
     }
 }
